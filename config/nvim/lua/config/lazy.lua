665637d5ccb99e8d5d9b35827c2be6aa80b80ddb
João Pedro Martins de França <mjoaopedro61@gmail.com>
2023-12-23 00:19:47 -0300

    :recycle: remove unused packages

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
     -- import/override with your plugins
     { import = "plugins" },
