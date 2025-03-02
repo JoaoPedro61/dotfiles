feaed41a471bc45417913e2c220f2c57e82b4d81
João Pedro Martins <mjoaopedro61@gmail.com>
2025-03-02 18:57:18 -0300

    feat(nvim): add a diagnostics popover

--- .config/nvim/lua/joaopedro61/autocmds.lua
+++ .config/nvim/lua/joaopedro61/autocmds.lua
@@ -35,3 +35,27 @@ autocmd("BufWritePre", {
     end
   end,
 })
+
+autocmd({ "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    if vim.diagnostic.is_enabled() then
+      for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if vim.api.nvim_win_get_config(winid).zindex then
+          return
+        end
+      end
+      vim.diagnostic.open_float({
+        scope = "cursor",
+        focusable = false,
+        close_events = {
+          "CursorMoved",
+          "CursorMovedI",
+          "BufHidden",
+          "InsertCharPre",
+          "WinLeave",
+        },
+      })
+    end
+  end,
+})
